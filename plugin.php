7a302585d9373cec8d8cd66002615528ebcdf982
Brandon <brandon@tendency.me>
2022-10-28 22:53:41 -0500

    fix(field): Fix default value return format (Fixes #26) (#40)
    * enhance(css): Add margin between the label and editor palette input

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: Advanced Custom Fields: Editor Palette Field
  * Plugin URI:  https://github.com/log1x/acf-editor-palette
  * Description: A Gutenberg-like editor palette color picker field for Advanced Custom Fields.
- * Version:     1.1.0
+ * Version:     1.1.1
  * Author:      Brandon Nifong
  * Author URI:  https://github.com/log1x
  */
