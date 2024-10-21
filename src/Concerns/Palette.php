fde4c27beba5a7839b61591a200bcbad7a769ce7
Dovydas Bumblauskas <dovydas.bumblauskas@consid.se>
2024-10-21 11:20:26 +0200

    Add check for array key

--- src/Concerns/Palette.php
+++ src/Concerns/Palette.php
@@ -44,8 +44,9 @@ trait Palette
             ]);
         }
 
-        return ! empty($color) && is_string($color) ? (
-            $colors[$color] ?? null
+        return ! empty($color) && is_string($color) && is_array($colors) ? (
+            array_key_exists($color, $colors) ? $colors[$color] : null
         ) : $colors;
+
     }
 }
